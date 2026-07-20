476fdc5733c1162522af160249904fb151c53d78
Gasile <bas.gasser@gmail.com>
2026-07-20 09:29:37 +0200

    migration(db): ajout des colonnes user_notifications et admin_notifications à la table Users

---
+++ citrineos-config/hasura/migrations/default/1784532281590_alter_table_public_Users_add_column_user_notifications/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."Users" add column "user_notifications" boolean
+ not null default 'true';
---
+++ citrineos-config/hasura/migrations/default/1784532306166_alter_table_public_Users_add_column_admin_notifications/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."Users" add column "admin_notifications" boolean
+ not null default 'true';
